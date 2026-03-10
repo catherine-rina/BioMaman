83a1e98f00937a0ee160bcdafb02ab02b4278de7
YONI-spec <agbodrejonathan@gmail.com>
2026-03-10 23:42:43 +0100

    encore un ajout

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 from pathlib import Path
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
+from tf_keras.models import load_model
 import time
 
 # ──────────────────────────────────────────────
